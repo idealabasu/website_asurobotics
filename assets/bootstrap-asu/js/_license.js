59f76129de5004ed81efa277a97a0df510a03ab4
Dan Aukes <danaukes@gmail.com>
2024-01-18 21:35:18 +0000

    cleanup

--- assets/bootstrap-asu/js/_license.js
+++
@@ -1,8 +0,0 @@
-/*!========================================================================
- * Web Standards: bootstrap-asu.js v1.0.1
- * ========================================================================
- * Copyright 2014-2016 ASU
- * Licensed under MIT (https://github.com/gios-asu/ASU-Web-Standards-Bootstrap/blob/master/LICENSE)
- * @license MIT
- * ========================================================================
-!*/
